fe3a41385084e57b96c3c1edc467c127c4547755
Lucas Augusto <lucas_lapada@hotmail.com>
2024-02-12 17:49:06 -0300

    Projeto com as funcionalidades do IMC e Historico pronto

---
+++ IMC/Model/CalcIMC.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.JSInterop;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+
+namespace IMC.Model
+{
+    public class CalcIMC
+    {
+      
+        public DateTime DataResultado { get; set; }
+        public double ValorIMC { get; set; }
+        public ImcStatus Status { get; set; }
+
+        //Metodos
+        public void CalculoDoIMC(pessoa pessoa)
+        {
+           ValorIMC = pessoa.Peso / (pessoa.Altura * pessoa.Altura);
+            if (pessoa.Idade)
+            {
+                if (ValorIMC <= 22)
+                    Status = ImcStatus.Magro;
+                else if (ValorIMC < 27)
+                    Status = ImcStatus.Normal;
+                else
+                    Status = ImcStatus.Sobrepeso;
+            }
+            else
+            {
+                if (ValorIMC <= 18.5)
+                    Status = ImcStatus.Magro;
+                else if (ValorIMC < 25)
+                    Status = ImcStatus.Normal;
+                else
+                    Status = ImcStatus.Sobrepeso;
+
+            }
+        }
+
+    }
+    public enum ImcStatus
+    {
+        Magro,
+        Normal, 
+        Sobrepeso,
+        Obesidade,
+        Obesidade_grave
+    }
+
+   
+}
